f5bc27f9c1c8a871d191e9e1361eab5eca65b9ea
Raibn <brayokara@gmail.com>
2026-02-18 19:21:45 +0300

    style strip down

--- core/models.py
+++ core/models.py
@@ -112,7 +112,7 @@ class Gallery(models.Model):
 
     def __str__(self):
         return self.title
-    
+# D   
 class Donation(models.Model):
     name = models.CharField(max_length=150)
     email = models.EmailField()
